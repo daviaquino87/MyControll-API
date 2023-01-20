93111d5fc08ffbe9b4e19e3e19455c2a9768ca23
daviaquino87 <davi26031@gmail.com>
2023-01-20 15:33:32 -0300

    feat: create entitie User

---
+++ src/modules/user/entities/User.ts
@@ -0,0 +1,90 @@
+import { Replace } from "helper/Replace";
+import { v4 as uuidV4 } from "uuid";
+
+interface IUserProps {
+  name: string;
+  email: string;
+  cpf: string;
+  birth_date: string;
+  password: string;
+  transactions: string;
+  categories: string;
+  created_At: Date;
+}
+
+export class User {
+  private _id: string;
+  private props: IUserProps;
+
+  constructor(props: Replace<IUserProps, { created_At?: Date }>, id?: string) {
+    (this._id = id ?? uuidV4()),
+      (this.props = {
+        ...props,
+        created_At: props.created_At ?? new Date(),
+      });
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public set name(name: string) {
+    this.props.name = name;
+  }
+
+  public get name(): string {
+    return this.props.name;
+  }
+
+  public set email(email: string) {
+    this.props.email = email;
+  }
+
+  public get email(): string {
+    return this.props.email;
+  }
+
+  public set cpf(cpf: string) {
+    this.props.cpf = cpf;
+  }
+
+  public get cpf(): string {
+    return this.props.cpf;
+  }
+
+  public set birth_date(birth_date: string) {
+    this.props.birth_date = birth_date;
+  }
+
+  public get birth_date(): string {
+    return this.props.birth_date;
+  }
+
+  public set password(password: string) {
+    this.props.password = password;
+  }
+
+  public get password(): string {
+    return this.props.password;
+  }
+
+  public set transactions(transactions: string) {
+    this.props.transactions = transactions;
+  }
+
+  public get transactions(): string {
+    return this.props.transactions;
+  }
+
+  public set categories(categories: string) {
+    this.props.categories = categories;
+  }
+
+  public get categories(): string {
+    return this.props.categories;
+  }
+
+  public get created_At(): Date {
+    return this.props.created_At;
+  }
+}
